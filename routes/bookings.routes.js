4b31e953621c3c31b80b616ae968f8166132de37
Lee Eunchan <ym4315@nate.com>
2023-07-18 09:23:18 +0900

    feat:예약 기능 기본설정

--- routes/bookings.routes.js
+++ routes/bookings.routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const BookingController = require('../controllers/bookings.controllers');
 const bookingController = new BookingController();
-router.post('/', bookingController.getbooking);
-router.post('/', bookingController.booking);
-
+router.get('/booking', bookingController.getBooking);
+router.post('/booking', bookingController.createBooking);
+router.put('/booking/bookingId', bookingController.updateBooking);
+router.delete('/booking/bookingId', bookingController.deleteBooking);
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersRouter = require('./users.routes');
-
+const bookingsRouter = require('./bookings.routes');
 router.use('/', usersRouter);
-
+router.use('/', bookingsRouter);
 module.exports = router;

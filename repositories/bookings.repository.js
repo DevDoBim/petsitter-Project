e2954046345a384db84cf8983bc521c7e67d4926
Lee Eunchan <ym4315@nate.com>
2023-07-19 09:09:12 +0900

    feat: 예약기능 생성까지 완료 테스트 필요

--- repositories/bookings.repository.js
+++ repositories/bookings.repository.js
@@ -1,5 +1,15 @@
 const { Bookings } = require('../models');
 
-class BookingRepository {}
-
+class BookingRepository {
+  getBooking = async () => {
+    await Bookings.findAll({});
+  };
+  createBooking = async (startDay, endDay) => {
+    const createBooking = await Bookings.create(startDay, endDay);
+    console.log('rep', createBooking);
+    return createBooking;
+  };
+  updateBooking = async () => {};
+  deleteBooking = async () => {};
+}
 module.exports = BookingRepository;

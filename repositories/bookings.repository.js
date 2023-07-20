d541c590beea794e11722ba0d0178ab1227713fa
Yeonbeom <dusqja14@naver.com>
2023-07-20 22:23:24 +0900

    Feat: 예약 수정 시 중복 확인 추가

--- repositories/bookings.repository.js
+++ repositories/bookings.repository.js
@@ -10,7 +10,7 @@ class BookingRepository {
   findSitter = async (petSitterId) => {
     return await Petsitters.findByPk(petSitterId);
   };
-  //예약 시간 중복 조회
+  //생성 시 예약 시간 중복 조회
   findExistBooking = async (petSitterId, startTime, endTime) => {
     return await Bookings.findOne({
       where: {
@@ -27,6 +27,17 @@ class BookingRepository {
   //예약 id로 조회
   getBookingById = async (bookingId) => {
     return await Bookings.findByPk(bookingId);
+  };
+  //수정 시 예약 시간 중복 조회
+  findExist = async (petSitterId, bookingId, startTime, endTime) => {
+    return await Bookings.findOne({
+      where: {
+        petSitterId,
+        bookingId: { [Op.not]: bookingId },
+        startTime: { [Op.lt]: endTime },
+        endTime: { [Op.gt]: startTime },
+      },
+    });
   };
   //예약 수정 API
   updateBooking = async (bookingId, startTime, endTime) => {
